e95c81f365ac805f13a6443168af77e0e7b9a9ee
chenyupeng <1141837983@qq.com>
2022-09-23 16:43:24 +0800

    完成了商品列表页面的开发

--- src/subpkg/goods_list/index.vue
+++ src/subpkg/goods_list/index.vue
@@ -1,21 +1,61 @@
 <template>
-  <div class="goods_list">goods_list</div>
+  <view>
+    <view class="goods-list">
+      <view
+        v-for="(goods, index) in goodsList"
+        :key="index"
+        @click="gotoDetail(goods)"
+      >
+        <myGoods :goods="goods"></myGoods>
+      </view>
+    </view>
+  </view>
 </template>
 
 <script>
+import myGoods from "../../components/my-goods/index";
 export default {
   name: "goods_list",
-  components: {},
-  props: {},
+  components: { myGoods },
   data() {
-    return {}
+    return {
+      queryObj: {
+        query: "",
+        cid: "",
+        pagenum: 1,
+        pagesize: 10,
+      },
+      goodsList: [],
+      total: 0,
+      isLoading: false,
+    };
   },
-  computed: {},
-  methods: {},
-  watch: {},
+  methods: {
+    async getGoodsList(cb) {
+      this.isLoading = true;
+      const { data: res } = await uni.$http.get(
+        "/api/public/v1/goods/search",
+        this.queryObj
+      );
+      this.isLoading = false;
+      cb && cb();
+      if (res.meta.status !== 200) return uni.$showMsg();
+      this.goodsList = [...this.goodsList, ...res.message.goods];
+      this.total = res.message.total;
+    },
+    gotoDetail(goods) {
+      uni.navigateTo({
+        url: "../../subpkg/goods_detail/index?goods_id=" + goods.goods_id,
+      });
+    },
+  },
 
   // 页面周期函数--监听页面加载
-  onLoad() {},
+  onLoad(options) {
+    this.queryObj.query = options.query || "";
+    this.queryObj.cid = options.cid || "";
+    this.getGoodsList();
+  },
   // 页面周期函数--监听页面初次渲染完成
   onReady() {},
   // 页面周期函数--监听页面显示(not-nvue)
@@ -25,14 +65,28 @@ export default {
   // 页面周期函数--监听页面卸载
   onUnload() {},
   // 页面处理函数--监听用户下拉动作
-  // onPullDownRefresh() { uni.stopPullDownRefresh(); },
+  onPullDownRefresh() {
+    this.queryObj.pagenum = 1;
+    this.total = 0;
+    this.isLoading = false;
+    this.goodsList = [];
+    this.getGoodsList(() => {
+      uni.stopPullDownRefresh();
+    });
+  },
   // 页面处理函数--监听用户上拉触底
-  // onReachBottom() {},
+  onReachBottom() {
+    if (this.queryObj.pagenum * this.queryObj.pagesize >= this.total)
+      return uni.$showMsg("数据加载完毕");
+    if (this.isLoading) return;
+    this.queryObj.pagenum += 1;
+    this.getGoodsList();
+  },
   // 页面处理函数--监听页面滚动(not-nvue)
   // onPageScroll(event) {},
   // 页面处理函数--用户点击右上角分享
   // onShareAppMessage(options) {},
-} 
+};
 </script>
 
-<style scoped></style>
+<style scoped lang="scss"></style>
